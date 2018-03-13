8ac71494ec70735442f7041f939668c955f907f5
李佳贵 <942513675@qq.com>
2018-03-13 18:18:56 +0800

    添加UIView+GGGestureRecognizer、UIButton+GGTitleAndImage，优化UIButton+GGTouchEvent

---
+++ GGCategory/GGCategory/UIButton/UIButton+GGTitleAndImage.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+GGTitleAndImage.m
+//  GGCategory
+//
+//  Created by GG on 2018/3/13.
+//  Copyright © 2018年 GG. All rights reserved.
+//
+
+#import "UIButton+GGTitleAndImage.h"
+
+@implementation UIButton (GGTitleAndImage)
+
+@end
---
+++ GGCategory/GGCategory/UIView/UIView+GGGestureRecognizer.m
@@ -0,0 +1,13 @@
+//
+//  UIView+GGGestureRecognizer.m
+//  GGCategory
+//
+//  Created by GG on 2018/3/13.
+//  Copyright © 2018年 GG. All rights reserved.
+//
+
+#import "UIView+GGGestureRecognizer.h"
+
+@implementation UIView (GGGestureRecognizer)
+
+@end
